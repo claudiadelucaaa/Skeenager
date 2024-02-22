6e55d7b0ba858ad13caa244099ccf542797ebbb7
Raffaele Marino <raffaelemarino04@gmail.com>
2024-02-22 11:47:40 +0100

    Home Page changes

--- skk/Skeenager/SwiftUI/HomePage.swift
+++ skk/Skeenager/SwiftUI/HomePage.swift
@@ -48,17 +48,66 @@ struct HomePage: View {
 }
 
 struct SelectYourProducts: View {
+    @State var isPresented = false
+    @State var isSelected = false
+    @State private var goalTypeFilter: steps? = nil
+    @State private var stepSelected: String = "CLEANSER"
+    @State private var showInfo: String = ""
+    
+    let steps = Steps()
+    let disabled: Color = .gray
     var body: some View {
+        
         VStack {
             Text("Select your products")
-                .font(.system(size: 30))
+                .font(.system(size: 40))
             Rectangle()
-                .frame(width: 300, height: 2)
-            
-        }.navigationBarBackButtonHidden(true)
+                .frame(width: 350, height: 2)
+            ForEach(steps.stepsList, id: \.self){
+                step in
+                Button(action: {
+                    isSelected.toggle()
+                    if isSelected {
+                        stepSelected = step.name
+                        showInfo = step.info
+                    }
+                }, label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color.black)
+                            .fill(goalTypeFilter == step ? step.color : disabled)
+                            .opacity(0.3)
+                        
+                        HStack {
+                            Text(step.name)
+                                .font(Font.custom("MyFont", size: 20))
+                            
+                            Spacer()
+                            
+                            Image(systemName: "info.circle")
+                            
+                        }.padding(.horizontal)
+                    }
+                    .onTapGesture {
+                        withAnimation(.linear(duration: 0.2)){
+                            if(goalTypeFilter == step){
+                                goalTypeFilter = nil
+                            }else{
+                                goalTypeFilter = step
+                            }
+                        }
+                    }
+                    
+                    .frame(width: 320.0, height: 60.0)
+                    .foregroundColor(.black)
+                    .padding(.horizontal)
+                    .padding(.vertical, 5)
+                })
+            }
+        }
     }
 }
 
 #Preview {
-    HomePage()
+    SelectYourProducts()
 }
